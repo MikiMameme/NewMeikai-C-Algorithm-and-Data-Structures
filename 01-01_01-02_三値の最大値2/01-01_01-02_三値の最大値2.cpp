93c0ba6b9fa8a5df5b3ad08efdc9bb0fd6d5f4a7
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-15 23:41:55 +0900

    01-01_01-02_三値の最大値2

---
+++ 01-01_01-02_三値の最大値2/01-01_01-02_三値の最大値2.cpp
@@ -0,0 +1,31 @@
+﻿/*三つの整数値の最大値を求める（すべての大小関係に対して確認）*/
+
+#include<stdio.h>
+
+int max3(int a, int b, int c)
+{
+	int  max = a;
+
+	if (b > max) max = b;
+	if (c > max) max = c;
+
+	return max;
+}
+
+int main(void)
+{
+	printf("max3(%d, %d, %d) = %d\n", 3, 2, 1, max3(3, 2, 1));
+	printf("max3(%d, %d, %d) = %d\n", 3, 2, 2, max3(3, 2, 2));
+	printf("max3(%d, %d, %d) = %d\n", 3, 1, 2, max3(3, 1, 2));
+	printf("max3(%d, %d, %d) = %d\n", 3, 2, 3, max3(3, 2, 3));
+	printf("max3(%d, %d, %d) = %d\n", 2, 1, 3, max3(2, 1, 3));
+	printf("max3(%d, %d, %d) = %d\n", 3, 3, 3, max3(3, 3, 3));
+	printf("max3(%d, %d, %d) = %d\n", 2, 2, 3, max3(2, 2, 3));
+	printf("max3(%d, %d, %d) = %d\n", 2, 3, 1, max3(2, 3, 1));
+	printf("max3(%d, %d, %d) = %d\n", 2, 3, 2, max3(2, 3, 2));
+	printf("max3(%d, %d, %d) = %d\n", 1, 3, 2, max3(1, 3, 2));
+	printf("max3(%d, %d, %d) = %d\n", 2, 3, 3, max3(2, 3, 3));
+	printf("max3(%d, %d, %d) = %d\n", 1, 2, 3, max3(1, 2, 3));
+
+	return 0;
+}
