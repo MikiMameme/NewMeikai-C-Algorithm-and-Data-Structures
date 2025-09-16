67a3d8f81b0750135e791325b82de584d94544b2
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-16 23:08:45 +0900

    01-02_01-05_1からnまでの整数の和を求める(for文)

---
+++ 01-02_01-05_1からnまでの整数の和を求める(for文)/01-02_01-05_1からnまでの整数の和を求める(for文).c
@@ -0,0 +1,26 @@
+﻿/*１、２・・・nの和を求める(for文)*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+int main(void)
+{
+	int i, n;
+	int sum;
+
+	puts("１からnまでの和を求めます");
+	printf("nの値 :");
+	scanf("%d", &n);
+
+	sum = 0;
+	i = 1;
+
+	for (i = 1; i <= n; i++) {
+		sum += i;
+	}
+
+	printf("1から%dまでの和は%dです\n", n, sum);
+
+	return 0;
+}
