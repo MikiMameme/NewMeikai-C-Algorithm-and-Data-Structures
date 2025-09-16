c410cf168b9b92d0b0410e64c8aab8902266f8e3
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-16 23:12:58 +0900

    01-02_01-06_正の値の読込み

---
+++ 01-02_01-06_正の値の読込み/01-02_01-06_正の値の読込み.c
@@ -0,0 +1,30 @@
+﻿/*１、２・・・nの和を求める(do文)*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+int main(void)
+{
+	int i, n;
+	int sum;
+
+	puts("１からnまでの和を求めます");
+	printf("nの値 :");
+	scanf("%d", &n);
+
+	do {
+		printf("nの値 :");
+		scanf("%d", &n);
+	} while (n <= 0);
+
+	sum = 0;
+
+	for (i = 1; i <= n; i++) {
+		sum += i;
+	}
+
+	printf("1から%dまでの和は%dです\n", n, sum);
+
+	return 0;
+}
