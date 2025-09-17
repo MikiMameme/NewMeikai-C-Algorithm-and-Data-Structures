77d763068e0a179b129a5295e73eb6dd268f16cd
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-17 18:18:56 +0900

    02-01_02-01_要素と添字

---
+++ 02-01_02-01_要素と添字/02-01_02-01_要素と添字.c
@@ -0,0 +1,23 @@
+﻿/*要素型がint型で要素数が5の配列*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+#define N 5
+
+int main()
+{
+	int i;
+	int a[N];
+
+	for (i = 0; i < N; i++) {
+		printf("a[%d] :", i);
+		scanf("%d", &a[i]);
+	}
+	puts("各要素の値");
+	for (i = 0; i < N; i++) {
+		printf("a[%d] = %d\n", i, a[i]);
+	}
+	return 0;
+}
