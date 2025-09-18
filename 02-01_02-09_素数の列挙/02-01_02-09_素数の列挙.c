0c2f85eed88cf978e57f421afbb4453f50ee7b78
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-18 21:26:32 +0900

    02-01_02-09_素数の列挙

---
+++ 02-01_02-09_素数の列挙/02-01_02-09_素数の列挙.c
@@ -0,0 +1,23 @@
+﻿/*1000以下の素数を列挙（その１）*/
+
+#include<stdio.h>
+
+int main(void)
+{
+	int i, n;
+	unsigned long counter = 0;
+
+	for (n = 2; n <= 1000; n++) {
+		for (i = 2; i < n; i++) {
+			counter++;
+			if (n % i == 0)
+				break;
+		}
+		if (n == 1)
+			printf("%d\n", n);
+	}
+
+	printf("除算を行った回数 :%lu\n", counter);
+
+	return 0;
+}
